ba6c76626a4ef495e04c84434b4bd4adddf71e61
Roman Kaluger <r.kaluger@sftpro.ru>
2023-07-23 13:39:34 +0300

    types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,3 @@
-export type AuthPayload = { login: string; password: string; id: string };
+export type AuthPayload = { login: string; password: string };
 export type RegisterPayload = AuthPayload;
-export type VerifyPayload = { token: string; id: string };
+export type VerifyPayload = { token: string };
