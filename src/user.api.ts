82f850bdf21e4cad1998825bc39132984f355944
Roman Kaluger <r.kaluger@sftpro.ru>
2023-12-23 23:00:09 +0300

    new api

--- src/user.api.ts
+++ src/user.api.ts
@@ -1,39 +1,15 @@
 import { userService } from "./user.service";
 import { ACTIONS } from "./actions";
-import { Socket } from "socket.io";
 import { RegisterPayload, VerifyPayload } from "./types";
-import { Request, SocketResponse, Api } from "@master_kufa/server-tools";
+import { Request, Api } from "@master_kufa/server-tools";
 
-export const userApi = new Api({
-  [ACTIONS.AUTH]: async (socket: Socket, payload: Request<RegisterPayload>) => {
-    const token = await userService.auth(payload);
-    const successResponse: SocketResponse<string> = {
-      requestId: payload.requestId,
-      payload: token,
-    };
+export const userApiHandlers = {
+  [ACTIONS.AUTH]: (payload: Request<RegisterPayload>) =>
+    userService.auth(payload),
+  [ACTIONS.REGISTER]: (payload: Request<RegisterPayload>) =>
+    userService.create(payload),
+  [ACTIONS.VERIFY]: (payload: Request<VerifyPayload>) =>
+    userService.verify(payload.token),
+};
 
-    socket.emit(ACTIONS.AUTH, successResponse);
-  },
-  [ACTIONS.REGISTER]: async (
-    socket: Socket,
-    payload: Request<RegisterPayload>,
-  ) => {
-    await userService.create(payload);
-    const successResponse: SocketResponse = {
-      requestId: payload.requestId,
-      payload: "success",
-    };
-
-    socket.emit(ACTIONS.REGISTER, successResponse);
-  },
-  [ACTIONS.VERIFY]: (socket: Socket, payload: Request<VerifyPayload>) => {
-    userService.verify(payload.token);
-
-    const successResponse: SocketResponse = {
-      requestId: payload.requestId,
-      payload: "success",
-    };
-
-    socket.emit(ACTIONS.VERIFY, successResponse);
-  },
-});
+export const userApi = new Api(userApiHandlers);
